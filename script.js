01d9f92836b491e9debcb51a51c0de37140d7469
Jordan De Visser <jordandevisser@gmail.com>
2021-02-20 16:17:31 +1000

    UI design elements added

--- script.js
+++ script.js
@@ -1,33 +1,61 @@
 let trackList = {
     001: [
-        'The Trial (Witcher 3)',
+        'THE TRIAL',
         'L74pTmqgQ9o',
         'COMBAT',
         'CHASE'
     ],
     002: [
-        'Aen Seidhe (Witcher 3)',
+        'AEN SEIDHE',
         'paDuue9Bgas',
         'INVESTIGATION',
         'INTRIGUE'
     ],
     003: [
-        'King Bran\'s Final Voyage (Witcher 3)',
+        'KING BRAN\'S FINAL VOYAGE',
         'r_gIRaYQRqo',
         'MOURNING',
         'LOSS'
     ]
 }
 
-function copyText(selection){
+function copyText(selection){ 
+    // Copy text from an off-screen text-area to clipboard.
     document.querySelector('#clipboard-textarea').value = `${selection}`;
     document.querySelector('#clipboard-textarea').select();
     document.execCommand("copy");
 }
 
+function populateTrackList() {
+    const listDiv = document.querySelector('#tracklist-container');
+
+    for (let i in trackList){
+        // Create radio buttons
+        let listEntry = document.createElement('input');
+        listEntry.setAttribute('type', 'radio');
+        listEntry.setAttribute('id', `${i}`);
+        listEntry.setAttribute('class', 'tracklist-entries');
+        listEntry.setAttribute('value', `${i}`);
+        listEntry.setAttribute('name', 'track-list');
+      
+        // Create labels
+        let entryLabel = document.createElement('label');
+        entryLabel.setAttribute('for', `${i}`);
+        entryLabel.setAttribute('class', 'tracklist-entries');
+        entryLabel.innerHTML = `${trackList[i][0]} [${trackList[i][2]}, ${trackList[i][3]}]`;
+      
+        // Build list
+        listDiv.append(listEntry);
+        listDiv.append(entryLabel);
+        listDiv.append(document.createElement('br'));
+    }
+  
+    document.getElementById('1').checked = true;
+}
+
 function copyTrack(){
     // Determine selection and then fetch trackID
-    let trackID;
+    let trackID = document.querySelector('input[name="track-list"]:checked').value;
     let url = trackList[trackID][1];
 
     document.querySelector('#clipboard-textarea').value = `p!play https://youtu.be/${url}`;
@@ -35,19 +63,21 @@ function copyTrack(){
     document.execCommand("copy");
 }
 
-function populateTrackList() {
-    const listDiv = document.querySelector('#tracklist-container')
+const stopButton = document.querySelector('#stop-button');
+const playButton = document.querySelector('#play-button');
+const pauseButton = document.querySelector('#pause-button');
+const skipButton = document.querySelector('#skip-button');
+const repeatAll = document.querySelector('#rep-all-button');
+const repeatOne = document.querySelector('#rep-one-button');
+const repeatOff = document.querySelector('#rep-off-button');
 
-    for (let i in trackList){
-        let listEntry = document.createElement('input');
-        listEntry.setAttribute('class', 'tracklist-entries');
-        listEntry.setAttribute('type', 'radio');
-        listEntry.setAttribute('name', `${i}`);
+stopButton.onclick = () => copyText('p!stop');
+playButton.onclick = () => copyText('p!resume');
+pauseButton.onclick = () => copyText('p!pause');
+skipButton.onclick = () => copyText('p!skip');
+repeatAll.onclick = () => copyText('p!repeat all');
+repeatOne.onclick = () => copyText('p!repeat one');
+repeatOff.onclick = () => copyText('p!repeat off');
 
-        let entryLabel = document.createElement('label');
-        entryLabel.setAttribute('for', `${i}`);
-        entryLabel.textContent = `${i}: ${trackList[i][0]}`;
-        listDiv.append(listEntry);
-        listDiv.append(entryLabel);
-    }
-}
+const copierButton = document.querySelector('#copier');
+copierButton.onclick = () => copyTrack();
